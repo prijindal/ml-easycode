f086585b3e3e880a6cdd25a2a1346a547fbb7a54
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-04-29 13:30:35 +0530

    Added test for search state

---
+++ client/src/store/search/index.test.js
@@ -0,0 +1,12 @@
+/* @flow */
+import reducer from './index';
+
+it('check initial state', () => {
+  expect(reducer()).toBe('');
+});
+
+it('should check changed state', () => {
+  expect(reducer('', { type: 'SET_SEARCH', search: 'Some Search' })).toBe(
+    'Some Search'
+  );
+});
